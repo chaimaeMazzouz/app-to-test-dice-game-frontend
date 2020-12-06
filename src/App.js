0c3f9a7818454e48f9dccd63a95d48631414f5ec
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2020-12-06 21:32:41 +0100

    still trying

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import React, { Component } from "react";
-import Player from "./Components/PlayerPanel/Player";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = initialState;
-  }
-  resetState = () => {
-    this.setState(initialState);
-  };
   render() {
-    const playerOne = 0;
-    const playerTwo = 1;
-    return (
-      <div>
-        <Player
-          id={playerOne}
-          active={this.state.activePlayer === playerOne ? "active " : ""}
-          score={this.state.playerOneScore}
-          current={this.state.playerOneCurrentScore}
-          winner={this.state.winner}
-        />
-        <Player
-          id={playerTwo}
-          active={this.state.activePlayer === playerTwo ? "active " : ""}
-          score={this.state.playerTwoScore}
-          current={this.state.playerTwoCurrentScore}
-          winner={this.state.winner}
-        />
-      </div>
-    );
+    return <div></div>;
   }
 }
 
---
+++ src/components/Controllers/Control.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Control() {
+  return <div></div>;
+}
+export default Control;
