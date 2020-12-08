3cb0535f0ed62ad7903d9b4e0980eb8438ab59bb
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2020-12-08 22:50:07 +0100

    save new stuff

--- src/components/StartPage/StartForm.js
+++ src/components/StartPage/StartForm.js
@@ -8,13 +8,17 @@ class StartForm extends Component {
     var button2 = document.getElementById("btn2");
     var button3 = document.getElementById("btn3");
     if (checkBox.checked == true) {
-      button3.style.display = "block";
-      button1.style.display = "none";
-      button2.style.display = "none";
-    } else {
       button3.style.display = "none";
       button1.style.display = "block";
       button2.style.display = "block";
+      document.getElementById("label-check").style.color = "#1e9feb";
+      document.getElementById("label-check").innerHTML = "Online";
+    } else {
+      button3.style.display = "block";
+      button1.style.display = "none";
+      button2.style.display = "none";
+      document.getElementById("label-check").style.color = "#cccccc";
+      document.getElementById("label-check").innerHTML = "Offline";
     }
   }
   render() {
@@ -25,10 +29,9 @@ class StartForm extends Component {
             <span>DICE</span>
             <span>GAME</span>
           </h1>
-          <label className="label-check">OnLine</label>
+          <span id="label-check"></span>
           <label className="switch">
             <input type="checkbox" id="myCheck" onClick={this.onSwitchClick} />
-
             <div className="slider" />
           </label>
         </header>
